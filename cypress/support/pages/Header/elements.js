a2a2ff03cc6b3caf7f8459dcebd12da28325eab1
juliano-cassimiro_elux <Juliano.cassimiro@qualitydigital.global>
2025-12-22 10:25:44 -0300

    fix: atualiza versão para 1.1.0 e refatora configuração do Cypress

--- cypress/support/pages/Header/elements.js
+++ cypress/support/pages/Header/elements.js
@@ -2,7 +2,8 @@ export const ELEMENTS = {
   inputSearchBar: '[data-testid="fs-search-input"] > [data-testid="fs-input"]',
   labelMsgInvalidLogin: 'div.vtex-login-2-x-formError',
   buttonLogin: '[data-testid="fs-link-button"] > div',
-  buttonLoginMobile: '[data-testid="fs-link"] > div > [data-testid="icon"]',
+  buttonLoginMobile:
+    '.LoginButton_loginButtonContainer__FAD_z > a > div > [data-testid="icon"]',
   buttonLoggedUser: '[data-testid="fs-link-button"] > div',
   buttonCart: '[data-testid="cart-toggle"] > [data-fs-button-wrapper="true"]',
 }
